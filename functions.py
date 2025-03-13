eb15c1bfb22270efe4fe2749822d52ea931cc39d
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2025-03-13 17:06:03 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -61,6 +61,103 @@ def preprocess(i, string, metric):
 
     return OP
 
+# Convert results into standard format
+
+@st.cache_data
+def convert_time(i, string, metric):
+
+    global output
+    
+    l=['discus', 'throw', 'jump', 'vault', 'shot']
+        
+    string=string.lower()
+
+    output=''
+    
+   # print('metric', metric)
+    
+    try:
+        
+        if 'w' in metric:  # skip marks with illegal wind speeds
+            
+            print('W', metric)
+            
+            output=''
+            
+        else:
+            
+    
+            if any(s in string for s in l)==True:
+            
+                if 'm' in metric:
+            
+                    metric=metric.replace('m', '')
+                    output=float(str(metric))
+            
+                elif 'GR' in metric:
+            
+                    metric=metric.replace('GR', '')
+                    output=float(str(metric))
+                
+                
+                else:
+    
+                    output=float(str(metric))
+        
+                
+                
+        
+            else:
+        
+                searchstring = ":"
+                searchstring2 = "."
+                substring=str(metric)
+                count = substring.count(searchstring)
+                count2 = substring.count(searchstring2)
+            
+                if count==0:
+                
+                    output=float(substring)
+            
+            
+               
+             
+                elif (type(metric)==datetime.time or type(metric)==datetime.datetime):
+                
+                                                
+                    time=str(metric)
+                    h, m ,s = time.split(':')
+                    output = float(datetime.timedelta(hours=int(h),minutes=int(m),seconds=float(s)).total_seconds())
+            
+                                
+                elif (count==1 and count2==1):
+            
+                    m,s = metric.split(':')
+                    output = float(datetime.timedelta(minutes=int(m),seconds=float(s)).total_seconds())
+                     
+                elif (count==1 and count2==2):
+                
+            
+                    metric = metric.replace(".", ":", 1)
+            
+                    h,m,s = metric.split(':')            
+                    output = float(datetime.timedelta(hours=int(h),minutes=int(m),seconds=float(s)).total_seconds())
+                
+        
+                elif (count==2 and count2==0):
+                
+            
+                    h,m,s = metric.split(':')
+                    output = float(datetime.timedelta(hours=int(h),minutes=int(m),seconds=float(s)).total_seconds())
+  
+            
+
+    except:
+        
+        pass
+                
+    return output
+
 
 # Clean each row of input file
 @st.cache_data
