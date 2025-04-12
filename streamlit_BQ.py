73eef82caff0e554aa0dab46b6ad6a83f757e085
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2025-04-12 16:16:47 +0800

    Update streamlit_BQ.py

--- streamlit_BQ.py
+++ streamlit_BQ.py
@@ -35,9 +35,9 @@ from google.cloud import bigquery
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
 )
-client = bigquery.Client(credentials=credentials)
+#client = bigquery.Client(credentials=credentials)
 
-storage_client = storage.Client()
+storage_client = storage.Client(credentials=credentials)
 
 blobs = storage_client.list_blobs(octc_athletes)
 
