9eb2aa29f610a5fb24e40da5e717a16844566adb
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2025-04-26 17:24:20 +0800

    Added get benchmark function

--- functions.py
+++ functions.py
@@ -675,8 +675,35 @@ def clean_columns(df):
 
                             
                  
-    
+@st.cache_data
+def get_benchmark(benchmark_option):
 
-       
+    if benchmark_option=='2023 SEAG Bronze':
+
+        benchmarks = client.query_and_wait(seag_benchmark_sql).to_dataframe()
+
+        benchmarks=benchmarks[benchmarks['HEAT'].isnull() & benchmarks['SUB_EVENT'].isnull()]  # r
+
+        benchmarks.reset_index(drop=True, inplace=True)
+        benchmarks.rename(columns = {'RESULT':'BENCHMARK'}, inplace = True)
+        benchmarks.drop(['YEAR', 'HEAT', 'NAME', 'RANK', 'CATEGORY_EVENT', 'COMPETITION', 'STAGE'], axis=1, inplace=True)
+    
+    
+    elif benchmark_option=='2025 Taiwan Open': 
+    
+        conn = st.connection('gcs', type=FilesConnection, ttl=600)
+    
+        benchmarks = conn.read("event_benchmarks/2025 Taiwan Open Benchmarks.csv", input_format="csv")
+    
+    else:
+        
+        conn = st.connection('gcs', type=FilesConnection, ttl=600)
+    
+        benchmarks = conn.read("event_benchmarks/26th Asian Athletics Benchmarks.csv", input_format="csv")
+
+    return benchmark
+    
+    
+           
     
 
