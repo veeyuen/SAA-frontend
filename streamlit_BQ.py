17773cc4ffd6c9bdfe1d33e929566fdd98e10d0f
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2025-04-26 19:50:44 +0800

    Update streamlit_BQ.py

--- streamlit_BQ.py
+++ streamlit_BQ.py
@@ -118,8 +118,7 @@ athletes_selected = data.loc[mask]
 benchmark_option = st.selectbox(
     "Please select performance benchmark",
     ("2023 SEAG Bronze", "26th Asian Athletics", "2025 Taiwan Open"),
-    index=None,
-    placeholder='Choose an option',
+    index='2023 SEAG Bronze',
 )
 
 
