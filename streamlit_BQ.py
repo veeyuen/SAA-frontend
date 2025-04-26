2348e3efdd0d5d3ed390135394fa21943780d822
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2025-04-26 19:21:51 +0800

    Update streamlit_BQ.py

--- streamlit_BQ.py
+++ streamlit_BQ.py
@@ -162,9 +162,9 @@ else:
 '''
 ## Convert benchmarks results to float64 compatible format ##
 
-process_benchmarks(benchmark) # call function to convert benchmark results to float64
+#process_benchmarks(benchmarks) # call function to convert benchmark results to float64
 
-st.write(benchmark)
+st.write(benchmarks)
 
 
 ## Prepare to merge benchmarks with athlete df ##
