34d7ad346be8e442a30664b8bdd32518e197b1f9
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2025-05-17 00:05:41 +0800

    Update streamlit_BQ.py

--- streamlit_BQ.py
+++ streamlit_BQ.py
@@ -108,7 +108,7 @@ athletes_selected = data.loc[mask]
 
 benchmark_option = st.selectbox(
     "Please Select Performance Benchmark (Select 'None' to Access All Records in Database)",
-    ("None - Direct Access to All Database Records", "2023 SEAG Bronze - SEAG Selection", "2023 SEAG Bronze - OCTC Selection", "26th Asian Athletics", "2025 Taiwan Open"),
+    ("None - Direct Access to All Database Records", "2023 SEAG Bronze - SEAG Selection", "2023 SEAG Bronze - OCTC Selection", "26th Asian Athletics", "2025 Taiwan Open", "2025 World Athletics Champs"),
 )
 
 if benchmark_option == 'None - Direct Access to All Database Records':
