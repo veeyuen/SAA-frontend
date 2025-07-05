b5818c370394b65524afd121435ef6be8d05e42a
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2025-07-05 10:53:48 +0800

    Update streamlit_BQ.py

--- streamlit_BQ.py
+++ streamlit_BQ.py
@@ -40,12 +40,12 @@ client = bigquery.Client(credentials=credentials)
 
 ## Read csv file containing list of foreigners ##
 @st.cache_data
-def fetch_foreigners():
+def foreigners():
     conn = st.connection('gcs', type=FilesConnection, ttl=600)
     foreigners = conn.read("name_lists/List of Foreigners.csv", encoding="utf-8", input_format="csv")
     return foreigners
 
-fetch_foreigners()  # get list of foreigners
+foreigners()  # get list of foreigners
 
 # Create list of foreigners 
 
