da56ac1fd19b03c70197e97c18db141c80683ca2
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2025-07-10 11:48:18 +0800

    Reinstated 'athletes_sql' query due to erros

--- streamlit_BQ.py
+++ streamlit_BQ.py
@@ -230,7 +230,7 @@ benchmarks = fetch_benchmarks()  # fetch benchmarks
 
 @st.cache_data(ttl=400)
 def fetch_data():
-    data = client.query_and_wait(athletes_map_sql).to_dataframe()
+    data = client.query_and_wait(athletes_sql).to_dataframe()
 
     data.dropna(how= "all", axis=1, inplace=True)
 
