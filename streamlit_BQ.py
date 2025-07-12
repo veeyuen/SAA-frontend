21474ea1bf3e5ac40aab2212086a3cffb0b196b2
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2025-07-12 20:37:03 +0800

    debugging

--- streamlit_BQ.py
+++ streamlit_BQ.py
@@ -298,9 +298,8 @@ mask = ((data['DATE'] >= start) & (data['DATE'] <= end))
 athletes_selected = data.loc[mask]
 athletes_selected.reset_index(drop=True, inplace=True)
 
-st.dataframe(athletes_selected)
 
-#st.dataframe(athletes_selected)
+st.dataframe(athletes_selected[athletes_selected['MAPPED_EVENT]=='')
 
 ## Allow public access via mito
 
