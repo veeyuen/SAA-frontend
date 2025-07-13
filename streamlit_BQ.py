83cc8885e21a6eb1bf98fd22363317484941ea51
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2025-07-13 18:32:40 +0800

    Update streamlit_BQ.py

--- streamlit_BQ.py
+++ streamlit_BQ.py
@@ -66,33 +66,32 @@ names = name_variations()
 
 # Create list of foreigners 
 
-foreigners['V1'] = foreigners['LAST_NAME']+' '+foreigners['FIRST_NAME']
-foreigners['V2'] = foreigners['FIRST_NAME']+' '+foreigners['LAST_NAME']
-foreigners['V3'] = foreigners['LAST_NAME']+', '+foreigners['FIRST_NAME']
-foreigners['V4'] = foreigners['FIRST_NAME']+' '+foreigners['LAST_NAME']
-
-for1 = foreigners['V1'].dropna().tolist()
-for2 = foreigners['V2'].dropna().tolist()
-for3 = foreigners['V3'].dropna().tolist()
-for4 = foreigners['V4'].dropna().tolist()
-
-foreign_list = for1+for2+for3+for4 
-
-foreign_list_casefold=[s.casefold() for s in foreign_list]
-
-exclusions = foreign_list_casefold
-
-st.write(exclusions)
-
 #foreigners['V1'] = foreigners['LAST_NAME']+' '+foreigners['FIRST_NAME']
 #foreigners['V2'] = foreigners['FIRST_NAME']+' '+foreigners['LAST_NAME']
 #foreigners['V3'] = foreigners['LAST_NAME']+', '+foreigners['FIRST_NAME']
 #foreigners['V4'] = foreigners['FIRST_NAME']+' '+foreigners['LAST_NAME']
-#foreign_list = pd.concat([
-#    foreigners['V1'], foreigners['V2'],
-#    foreigners['V3'], foreigners['V4']
-#]).dropna().str.casefold().unique().tolist()
-#exclusions = set(foreign_list)
+
+#for1 = foreigners['V1'].dropna().tolist()
+#for2 = foreigners['V2'].dropna().tolist()
+#for3 = foreigners['V3'].dropna().tolist()
+#for4 = foreigners['V4'].dropna().tolist()
+
+#foreign_list = for1+for2+for3+for4 
+
+#foreign_list_casefold=[s.casefold() for s in foreign_list]
+
+#exclusions = foreign_list_casefold
+
+
+foreigners['V1'] = foreigners['LAST_NAME']+' '+foreigners['FIRST_NAME']
+foreigners['V2'] = foreigners['FIRST_NAME']+' '+foreigners['LAST_NAME']
+foreigners['V3'] = foreigners['LAST_NAME']+', '+foreigners['FIRST_NAME']
+foreigners['V4'] = foreigners['FIRST_NAME']+' '+foreigners['LAST_NAME']
+foreign_list = pd.concat([
+    foreigners['V1'], foreigners['V2'],
+    foreigners['V3'], foreigners['V4']
+]).dropna().str.casefold().unique().tolist()
+exclusions = set(foreign_list)
 
 ### DEFINE SQL QUERIES ###
 
@@ -462,6 +461,8 @@ if benchmark_option != 'None - Direct Access to All Database Records':
 # Remove foreigners
 
     df = df.loc[~df['NAME'].str.casefold().isin(exclusions)]  # ~ means NOT IN. DROP spex carded athletes
+
+    st.dataframe(df)
 
 
 # Choose the best result for each event participated by every athlete
