755d54905d0178dfcd133ffcce08f179ab02b871
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2025-07-29 18:24:44 +0800

    Update streamlit_BQ.py

--- streamlit_BQ.py
+++ streamlit_BQ.py
@@ -327,7 +327,7 @@ if benchmark_option == 'Search Database Records by Name or Competition':
         name_selected = st.multiselect('Select From Search Results:', all_data.loc[all_data['NAME_case'].str.contains(text)]['NAME'].unique())
 
       #  st.write(name_selected[0])
-        m1 = all_data["NAME_case"].str.contains(name_selected[0])
+        m1 = all_data["NAME"].str.contains(name_selected[0])
 
         
         #all_data.loc[all_data['NAME_case'].str.contains(text)]['NAME_case'].unique()
