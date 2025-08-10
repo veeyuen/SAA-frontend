539ba6d865678965b61b962faa9abdc1914152e7
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2025-08-10 21:23:51 +0800

    Putting back mitosheet

--- streamlit_BQ.py
+++ streamlit_BQ.py
@@ -252,7 +252,8 @@ if benchmark_option == 'Search Database Records by Name or Competition':
     
 
         if text_search:
-            st.write(df_search)
+      #      st.write(df_search)
+            final_dfs, code = spreadsheet(df_search)
 
         all_data.drop(['NAME_case'], axis=1, inplace=True)
 #
