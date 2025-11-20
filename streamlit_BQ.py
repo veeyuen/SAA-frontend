2340b2b06f40198e767cc1b949c9aa84f36eb814
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2025-11-20 22:21:24 +0800

    Update streamlit_BQ.py

--- streamlit_BQ.py
+++ streamlit_BQ.py
@@ -13,7 +13,7 @@ import pytz
 from streamlit_gsheets import GSheetsConnection
 from st_files_connection import FilesConnection
 from functions import convert_time, process_results, map_international_events, clean_columns, simple_map_events, normalize_text
-from functions import normalize_time_format, convert_time_refactored, convert_time_format
+from functions import normalize_time_format, convert_time_refactored_2, convert_time_format
 from google.cloud import storage
 from mitosheet.streamlit.v1 import spreadsheet
 
