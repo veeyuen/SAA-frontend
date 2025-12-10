3d7ed822a35aef65d0dbe95dffaa3d9f0205b3cd
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2025-12-10 15:41:21 +0800

    Convert RESULT_C to float and format NAME
    Convert RESULT_C column to float type and update NAME formatting.

--- streamlit_BQ.py
+++ streamlit_BQ.py
@@ -440,6 +440,9 @@ if benchmark_option == 'Search Database Records by Name or Competition':
 # used in the data ('DNF' vs 'dnf').
         df_search.loc[mask_non_numeric, 'RESULT_C'] = df_search['RESULT']
 
+        df_search['RESULT_C'] = df_search['RESULT_C'].astype(float)
+
+
         df_final = df_search[['NAME', 'DATE', 'MAPPED_EVENT', 'COMPETITION', 'RESULT_C', 'RESULT_CONV', 'WIND', 'HOST_CITY', 'AGE', 'GENDER', 'EVENT_CLASS', 'DOB']]  #NEW
 
         df_final['NAME'] = df_final['NAME'].fillna('').str.title() # Capitalize Name
