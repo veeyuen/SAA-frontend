bf613870d62448f549579ad0bc850d771f967c2a
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2025-12-10 09:01:16 +0800

    Update streamlit_BQ.py

--- streamlit_BQ.py
+++ streamlit_BQ.py
@@ -503,7 +503,7 @@ elif benchmark_option == 'List Results By Event':
     
     distance_events = ['60m', '60m Hurdles', '80m', '100m', '100m Hurdles', '110m Hurdles', '400m Hurdles', '200m', '400m', '800m', '2400m', '10,000m', '3000m', '5000m', 
                         '3000m Steeplechase', '1500m', '10000m Racewalk', '20km Racewalk', '1 Mile', '4 x 100m', '4 x 400m', '2000m Steeplechase', 'Marathon',
-                        'Sprint Medley Relay', '5km Racewalk', '200m Hurdles']
+                        'Sprint Medley Relay', '5km Racewalk', '200m Hurdles', '5000m Racewalk', '10,000m Racewalk']
 
 # 2. Rewrite the conditional statement
     #if list_option in distance_events:
